f44a2c37ea609f826da651acc4207b694ca8ed89
sethclaw <sethclaw10@gmail.com>
2017-02-08 15:17:24 -0700

    fixed formatting and playing errors. TODO: add instructions.

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
 
 	
 	$('#searchButton').click(function() {
-		let query = "https://api.spotify.com/v1/search?q=" + $('#artistField').val() + "&type=artist&limit=10";
+		let query = "https://api.spotify.com/v1/search?q=" + $('#artistField').val() + "&type=artist&limit=25";
 		$.getJSON(query, function(data) {
 			let artistsData = data.artists.items;
 			
@@ -42,13 +42,33 @@ $(document).ready(function() {
 					b: 200,
 					t: 100
 				},
+				marker: {         // marker is an object, valid marker keys: #scatter-marker
+            		color: 'rgb(36, 255, 55)' // more about "marker.color": #scatter-marker-color
+				},
 				autosize:true
 			}];
 
 			$('#results').show();
 
 			Plotly.newPlot('plot', plotSettings);
+			
 			$("#previews").html("");
+			$("#previews").css("display", "-webkit-flex");
+			$("#previews").css("display", "flex");
+			$("#previews").css("-webkit-align-items", "center");
+			$("#previews").css("align-items", "center");
+			$("#previews").css("-webkit-justify-content", "center");
+			$("#previews").css("justify-content", "center");
+
+			$("#previews").css("-webkit-flex-direction", "row");
+			$("#previews").css("flex-direction", "row");
+			$("#previews").css("-webkit-flex-wrap", "wrap");
+			$("#previews").css("flex-wrap", "wrap");
+			$("#previews").css("-webkit-flex-flow", "row wrap");
+			$("#previews").css("flex-flow", "row wrap");
+			$("#previews").css("-webkit-align-content", "flex-end");
+			$("#previews").css("align-content", "flex-end");
+			
 			console.log(imageInfo);
 			$.each(imageInfo, function(index, image) {
 				if(image !== undefined &&
@@ -56,22 +76,34 @@ $(document).ready(function() {
 				  image.src.url !== undefined &&
 				  image.id !== undefined &&
 				  image.name !== undefined){
-					var d = document.createElement('div');
-					$(d).attr({
-						'id': image.id,
-						'class': "cover",
-						'style': "background: url('" + image.src.url +"');margin: 20px; height:200px; width:200px; background-size: 80%; repeat: false;"
-					});
-					$(d).addClass("cover");
-					$(d).appendTo("#previews");
+					var d = $(document.createElement('div'));
+					var i = $(document.createElement('img'));
+					var p = $(document.createElement('p'));
+					d.css("margin", "20px");
+					i.attr("id",image.id);
+					i.addClass('cover');
+					i.attr('src',image.src.url);
+					i.attr('width','200px');
+					p.text(image.name);
+					i.appendTo(d);
+					p.appendTo(d);
+					d.appendTo($("#previews"));
 				}
 			});
 		});
 	});
 	$(document).on('click', '.cover',  function (e) {
+		//restart audio or start playing audio on click
 		if(audioObject !== undefined){
 			audioObject.pause();	
 		}
+		
+		var id = e.target.id;
+		console.log(e.target);
+		if(e.target.id === ""){
+			id = e.target.parentElement.attributes.id;
+		}
+		console.log(e);
 		console.log(e.target.id);
 		let query = "https://api.spotify.com/v1/artists/" + e.target.id + "/top-tracks?country=US";
 		$.getJSON(query,function(data){
@@ -79,5 +111,14 @@ $(document).ready(function() {
 			audioObject = new Audio(data.tracks[0].preview_url);
 			audioObject.play();
 		});
+		$(".playing").attr("class","cover"); 
+		$("#" + e.target.id).attr("class","playing");
+	});
+	$(document).on('click', '.playing',  function (e) {
+		if(audioObject !== undefined){
+			audioObject.pause();	
+		}
+		console.log(e.target.id);
+		$("#" + e.target.id).attr('class', 'cover');
 	});
 });
